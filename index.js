77ff96bce755c89874afeece1e8cceec4c2c47d9
ylcnn <yalcnnemre@gmail.com>
2023-10-24 15:01:25 +0300

    abc

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get("/naber", (req, res) => {
     res.send("iyidir sahip sen nasılsın")
 })
 
+app.get("/test",(req,res)=>{
+    res.send("selam bebek mugo ben kelebek")
+})
+
 
 app.listen(5000, () => {
     console.log("server is running")
